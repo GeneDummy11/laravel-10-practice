55e371c4811a7e934be4ce36b04f94df8e748eda
Genesis Esquilona <gtesquilona@gtesquilona1804.PPPCenter.LAN>
2024-04-17 15:33:59 +0800

    Updated controller and created a new request class to handle avatar input validation

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Profile;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAvatarRequest;
 
 class AvatarController extends Controller
 {
-    public function update(Request $request)
+    public function update(UpdateAvatarRequest $request)
     {
-        dd($request->input('avatar'));
+        dd($request->all());
     }
 }
