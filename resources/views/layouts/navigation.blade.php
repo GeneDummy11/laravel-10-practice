e9f74d7e3308228b3e9d7fce1cb2ff102c08b525
Genesis Esquilona <gtesquilona@gtesquilona1804.PPPCenter.LAN>
2024-04-22 15:37:25 +0800

    Change navigation dropdown from user name to user avatar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,8 +23,8 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>
-                                {{ Auth::user()->name }}
+                            <div class="flex-auto">
+                                <img class="w-10 h-10 rounded-full" src={{ "/storage/".Auth::user()->avatar }} alt="user avatar" />
                             </div>
 
                             <div class="ms-1">
